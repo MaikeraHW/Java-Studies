ba06f22d9ecad2f536185e1a8a314dc7a3dea428
Maicon Henrique Wagner <wagner.maiconhenrique@gmail.com>
2026-04-14 08:22:44 -0300

    polimorfism classes add with excercises

--- Polimorfism.java
+++ Polimorfism.java
@@ -1,30 +1,42 @@
 abstract  class Animal { //Superclasse
+    String name;
+
+    public Animal(String nameInt){
+        this.name = nameInt;
+    }
 
     public abstract void fazerSom();
+
+    public abstract void caminhar();
     
 }
 
 
 class Cachorro extends Animal { //SubClasse
+    String tipoLatido;
+
+    public Cachorro(String name, String tipoLatidoInt){
+        super(name);
+        this.tipoLatido = tipoLatidoInt;
+    }
+
     public void fazerSom() {
-        System.out.println("O Cachorro faz Au au!");
+        System.out.println("O " + name + " está latindo " + tipoLatido);
+    }
+
+    public void caminhar(){
+        System.out.println("O " + name + " está caminhando");
     }
     
-}
-
-class Gato extends Animal { //Subclasse
-    public void fazerSom(){
-        System.out.println("O gato faz miau miau!");
-    }
-
 }
 
 public class Polimorfism {
     public static void main(String[] args) {
         
-        Animal myDog = new Cachorro();
+        Animal myDog = new Cachorro("Rex", "Grave");
             myDog.fazerSom();
-        Animal myCat = new Gato();
-            myCat.fazerSom();
+            myDog.caminhar();
     }
 }
+
+//Polimorfismo: Declara com a classe pai porém cria a instância na classe filha
